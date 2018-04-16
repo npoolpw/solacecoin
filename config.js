2b67e6ca8c5e8e77c7a82d921ace100dd2129f43
npool.pw <36919430+npoolpw@users.noreply.github.com>
2018-04-16 09:47:24 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,24 @@
-var api = "http://apihost.com:port";
+var api = "http://ca.npool.pw:2300";
 
-var poolHost = "apihost.com";
+var poolHost = "ca.npool.pw";
 
-var blockExplorer = "http://blockexplorer.pw/coin";
+var blockExplorer = "http://explorer.solace-coin.com/";
 
 var cryptonatorWidget = ["BTC-USD", "XMR-USD"];
 
 var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
 
-var blockchainExplorer = "http://blockexplorer.pw/{symbol}/?hash={id}#blockchain_block";
+var blockchainExplorer = "http://explorer.solace-coin.com/?hash={id}#blockchain_block";
 
-var transactionExplorer = "http://blockexplorer.pw/{symbol}/?hash={id}#blockchain_transaction";
+var transactionExplorer = "http://explorer.solace-coin.com/?hash={id}#blockchain_transaction";
 
 var themeCss = "themes/default-theme.css";
 
 var networkStat = [
-        ["coin.apihost.com", "http://coin.apihost.com:port"]
+        ["npool.pw/solacecoin", "http://ca.npool.pw:2300"],
+        ["pool.solace-coin.com", "https://pool.solace-coin.com:8882"],
+        ["solace.luckypool.io", "https://solace.luckypool.io/api"],
+        ["solacecoin.cryptonote.host/solacecoin", "http://185.151.29.95:8118"],
+        ["solace.saltpool.net", "http://solace.saltpool.net:8124"],
+        ["pool.pocketwhale.info/solace", "http://pool.pocketwhale.info:8117"]
 ];
